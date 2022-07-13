2c0ec1263b0c12f658bdeb25e48d1dfd189f238a
huang_junbo <huangjunbo1107@outlook.com>
2022-07-13 20:17:18 +0800

    最终可运行版本

--- OCR.py
+++ OCR.py
@@ -15,7 +15,7 @@ class Application(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.title("图片文本提取")
+        self.title("Image text extraction")    #图片文本提取
 
         pt.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
         self.img_path = tk.StringVar()
@@ -23,26 +23,26 @@ class Application(tk.Tk):
         self.frame = tk.Frame(self)
         self.frame.pack(padx=10, pady=10)
 
-        self.lbl_file = tk.Label(self.frame, text="图像")
+        self.lbl_file = tk.Label(self.frame, text="Image")
         self.lbl_file.grid(row=0, column=0)
 
         self.txt_file = tk.Entry(self.frame, width=50, textvariable=self.img_path)
         self.txt_file.grid(row=0, column=1, sticky=tk.W)
 
-        self.btn_file = tk.Button(self.frame, text="选择", command=self.sel_img_file)
+        self.btn_file = tk.Button(self.frame, text="choose", command=self.sel_img_file)
         self.btn_file.grid(row=0, column=1, sticky=tk.E)
 
-        self.lbl_txt = tk.Label(self.frame, text="文本")
+        self.lbl_txt = tk.Label(self.frame, text="text")
         self.lbl_txt.grid(row=1, column=0)
 
         self.txt_exract = tk.Text(self.frame)
         self.txt_exract.grid(row=1, column=1)
 
-        self.btn_extract = tk.Button(self.frame, text="提取文本", command=self.extract_text)
+        self.btn_extract = tk.Button(self.frame, text="extract text", command=self.extract_text)
         self.btn_extract.grid(row=2, column=1, sticky=tk.W + tk.E)
 
     def sel_img_file(self):
-        self.img_path.set(filedialog.askopenfilename(title="选择图片", initialdir="."))
+        self.img_path.set(filedialog.askopenfilename(title="Select Image", initialdir="."))
 
     def extract_text(self):
         if self.img_path:
